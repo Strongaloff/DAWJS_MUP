03e1325acb3bb9e2d398fca1387ce8b44c8d3818
J0ck3rul <eduardtibuleac@gmail.com>
2020-02-11 04:45:39 +0200

    mai avem 3 ore

--- source/back-end/party-manager/controllers/partyController.js
+++ source/back-end/party-manager/controllers/partyController.js
@@ -2,7 +2,8 @@ var accountDb = require("../database/party-db");
 var userDb = require("../database/user.db");
 exports.createParty = (req, res) => {
   partyObj = req.body;
-
+  console.log(partyObj);
+  
   accountDb.find(partyObj).then(result => {
     if(result !== null) {
       res.status(403).send("already exists");
